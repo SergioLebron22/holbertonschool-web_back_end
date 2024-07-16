ad4acb982dde633aa10e1ae065bdced178b6eed2
SergioLebron22 <lebronsergio22@gmail.com>
2024-07-16 13:52:03 -0400

    added rest parameter function

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,6 +1,6 @@
 export default function returnHowManyArguments(...input) {
   let count = 0;
-  for (let i = 0; i < input.length; i+=1) {
+  for (let i = 0; i < input.length; i += 1) {
     count += 1;
   }
   return count;
