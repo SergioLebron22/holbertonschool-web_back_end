87a5b1be758d63e227f2805a16023d46191480e3
SergioLebron22 <lebronsergio22@gmail.com>
2024-07-19 16:47:22 -0400

    feat: Refactor Building class constructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,14 @@
 export default class Building {
-    constructor(sqft) {
-        if (this.constructor !== Building && !this.evacuationWarningMessage) {
-            throw TypeError("Class extending Building must override evacuationWarningMessage");
-        }
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw TypeError('Class extending Building must override evacuationWarningMessage');
     }
+    /* eslint-disable no-underscore-dangle */
 
-    get sqgt() {
-        return this._sqft;
-    }   
+    this._sqft = sqft;
+  }
+
+  get sqgt() {
+    return this._sqft;
+  }
 }
-
