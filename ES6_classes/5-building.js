03797f839a7e984469f1a8ff177fc1be1719200b
SergioLebron22 <lebronsergio22@gmail.com>
2024-07-19 16:53:07 -0400

    feat: Add type validation for sqft in Building class constructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -4,7 +4,9 @@ export default class Building {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     /* eslint-disable no-underscore-dangle */
-
+    if (typeof (sqft) !== 'number') {
+      throw TypeError('sqft must ba a number');
+    }
     this._sqft = sqft;
   }
 
