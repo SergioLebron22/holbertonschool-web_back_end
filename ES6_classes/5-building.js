fb06562023a19f9b86f9c6f144921669c0200a6d
SergioLebron22 <lebronsergio22@gmail.com>
2024-07-19 16:50:49 -0400

    Refactor Building class constructor error handling

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
-      throw TypeError('Class extending Building must override evacuationWarningMessage');
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
     /* eslint-disable no-underscore-dangle */
 
