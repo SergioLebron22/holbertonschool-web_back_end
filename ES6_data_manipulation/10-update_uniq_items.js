a86b4e737e44eb8c7a252e945b52163cc0462fec
SergioLebron22 <lebronsergio22@gmail.com>
2024-07-23 16:48:39 -0400

    Update unique items in map to have a value of 100 if their current value is 1

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error("Cannot process");
+  }
   for (const [key] of map) {
     if (map.get(key) === 1) map.set(key, 100);
   }
