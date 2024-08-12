391ff6481d6f9d3f3ec5d31464867662a3f64faf
SergioLebron22 <lebronsergio22@gmail.com>
2024-08-12 15:18:38 -0400

    Add type annotations to the add function for better code clarity and maintainability

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This function takes two float numbers as input and returns their sum.
+
+Parameters:
+    a (float): The first number.
+    b (float): The second number.
+
+Returns:
+    float: The sum of the two numbers.
+"""
+
+def add(a: float, b: float) -> float:
+    return a + b
