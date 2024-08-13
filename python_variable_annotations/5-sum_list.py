aac2b2b97d3e273feaa70d427ec5df0bdcd6c782
SergioLebron22 <lebronsergio22@gmail.com>
2024-08-13 10:53:29 -0400

    Refactor sum_list function to use List[float] type annotation for better code clarity and maintainability

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+from typing import List
+
 """this function sums all floats in a given list"""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """returns the sum of all floats in the list"""
     add: float = 0.0
     for num in input_list:
