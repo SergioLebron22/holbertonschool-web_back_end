fcd8fa66e2706091792028963ebe928d6e1aa9e9
SergioLebron22 <lebronsergio22@gmail.com>
2024-08-13 12:21:50 -0400

    Refactor to_kv function to square the value parameter for better code clarity and maintainability

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -7,5 +7,5 @@ from typing import Tuple, Union
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """returns a tuple where k is str and v is either float or int"""
-    tup: Tuple[str, float] = (k, v)
+    tup: Tuple[str, float] = (k, v**2)
     return tup
