5d37c8fd55c8d34e12e3d07f3f394f201d49f0cf
SergioLebron22 <lebronsergio22@gmail.com>
2024-08-13 13:07:16 -0400

    Refactor element_length function to use type annotations and add docstring for better code clarity and maintainability

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+"""this file contains the element_length function"""
 
 from typing import Iterable, List, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """returns a list of tuples"""
     return [(i, len(i)) for i in lst]
