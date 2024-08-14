a8fdc8bc94c736f7250befea0474f9605adfdd1c
SergioLebron22 <lebronsergio22@gmail.com>
2024-08-14 12:16:02 -0400

    chore: Add basic async syntax code and README for python async functions

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""this async coroutine takes an int and waits for random delay"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10) -> float:
+    """returns random delay"""
+    rand_delay = random.uniform(0, max_delay)
+    await asyncio.sleep(rand_delay)
+    return rand_delay
