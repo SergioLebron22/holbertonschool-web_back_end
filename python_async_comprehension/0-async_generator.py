92bf7b1989a69c1c27f8533252cd21cfb3022c7c
SergioLebron22 <lebronsergio22@gmail.com>
2024-08-19 19:44:32 -0400

    chore: Add async generator for Python asyncio

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""This file contains an async generator"""
+
+import random
+from typing import List
+import asyncio
+
+
+async def async_generator():
+    """yields a random number from 0 through 10"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
