c6b2425aacf72291dc206b553836277b26eb8e33
SergioLebron22 <lebronsergio22@gmail.com>
2024-08-19 21:32:28 -0400

    chore: Update async_generator function with return type annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ from typing import Generator
 import asyncio
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """yields a random number from 0 through 10"""
     for _ in range(10):
         await asyncio.sleep(1)
