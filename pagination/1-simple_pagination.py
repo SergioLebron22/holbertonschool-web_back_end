aadf318fc2872b4ed0a18938a7d827b2ae0d2058
SergioLebron22 <lebronsergio22@gmail.com>
2024-08-21 10:49:40 -0400

    Fix index_range method in simple pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -35,6 +35,7 @@ class Server:
 
         return data[start_index:end_index]
 
+    @staticmethod
     def index_range(self, page, page_size) -> Tuple(int, int):
         """this function returns a tuple with the start and end indexes"""
         start_index = (page - 1) * page_size
