634fc9e7c5910e169c803f99757813959d1e14b1
SergioLebron22 <lebronsergio22@gmail.com>
2024-08-22 11:34:04 -0400

    feat: Add schools_by_topic function to retrieve a list of schools by topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""This file contains the function schools_by_topic"""
+
+def schools_by_topic(mongo_collection, topic):
+    """returns a list"""
+    return list(mongo_collection.find({"topic": topic}))
