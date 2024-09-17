67ad78ef1a4275d63c96301e14ac66f6039a7dcb
SergioLebron22 <lebronsergio22@gmail.com>
2024-09-17 14:15:16 -0400

    Refactor countStudents function to read and parse a CSV file in Node.js

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+function countStudents(path) {
+  if (path !== 'database.csv') {
+    throw new Error('Cannot load the database');
+  }
+  let count = 0;
+  const listCS = [];
+  const listSWE = [];
+  fs.createReadStream(path)
+    .pipe(parse({ delimiter: ',', from_line: 2 }))
+    .on('data', (row) => {
+      count += 1;
+      if (row[3] === 'CS') listCS.push(row[0]);
+      else if (row[3] === 'SWE') listSWE.push(row[0]);
+    })
+    .on('end', () => {
+      console.log(`Number of students: ${count}`);
+      console.log(`Number of students in CS: ${listCS.length}. List: ${listCS.join(', ')}`);
+      console.log(`Number of students in SWE: ${listSWE.length}. List: ${listSWE.join(', ')}`);
+    });
+}
+
+module.exports = countStudents;
