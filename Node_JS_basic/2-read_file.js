6c457b22ef1a5c463b00c5f6b8705f0267d0e333
SergioLebron22 <lebronsergio22@gmail.com>
2024-09-17 16:48:17 -0400

    Refactor countStudents function to read and parse a CSV file in Node.js

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,25 +1,30 @@
 const fs = require('fs');
-const csv = require('csv');
 
 function countStudents(path) {
   if (path !== 'database.csv') {
     throw new Error('Cannot load the database');
   }
-  let count = 0;
+
   const listCS = [];
   const listSWE = [];
-  fs.createReadStream(path)
-    .pipe(csv.parse({ delimiter: ',', from_line: 2 }))
-    .on('data', (row) => {
-      count += 1;
+
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const rows = data.trim().split('\n');
+
+    for (let i = 1; i < rows.length; i += 1) {
+      const row = rows[i].split(',');
+
       if (row[3] === 'CS') listCS.push(row[0]);
       else if (row[3] === 'SWE') listSWE.push(row[0]);
-    })
-    .on('end', () => {
-      console.log(`Number of students: ${count}`);
-      console.log(`Number of students in CS: ${listCS.length}. List: ${listCS.join(', ')}`);
-      console.log(`Number of students in SWE: ${listSWE.length}. List: ${listSWE.join(', ')}`);
-    });
+    }
+
+    console.log(`Number of students: ${rows.length - 1}`);
+    console.log(`Number of students in CS: ${listCS.length}. List: ${listCS.join(', ')}`);
+    console.log(`Number of students in SWE: ${listSWE.length}. List: ${listSWE.join(', ')}`);
+  });
 }
 
 module.exports = countStudents;
