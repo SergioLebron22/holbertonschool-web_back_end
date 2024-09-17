757eaccad1e1a0fca972862563fe1bdaf56c5cf3
SergioLebron22 <lebronsergio22@gmail.com>
2024-09-17 09:39:24 -0400

    Refactor console.log statement to include carriage return in Node.js script

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,6 +2,6 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+  console.log(`Your name is: ${name}\r`);
   process.exit();
 });
