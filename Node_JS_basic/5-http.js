0688fe2e5dfdae80c215433997ca45d9e8fcbe3c
SergioLebron22 <lebronsergio22@gmail.com>
2024-09-19 12:04:49 -0400

    Refactor error handling in 5-http.js and improve code readability

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,28 +1,25 @@
-const http =  require('http');
+const http = require('http');
 const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    const { url } = req;
-    const path = process.argv[2];
-    if (url === '/') {
-        res.end("Hello Holberton School!");
-    }
-    else if (url === '/students') {
-        res.write('This is the list of our students\n');
-        countStudents(path).then((data) => {
-            const str = data.join('\n');
-            res.end(str);
-        });
-        //.catch((err) => {
-        //     console.log(err)
-        //     res.write(err)
-        //     res.end()
-        // });
-    }
-    else {
-        res.end('Not Found')
-    }   
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  const { url } = req;
+  const path = process.argv[2];
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(path)
+      .then((data) => {
+        const str = data.join('\n');
+        res.end(str);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    res.end('Not Found');
+  }
 });
 
 app.listen(1245);
